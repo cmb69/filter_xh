846b9690b4dc423fc6ee59b6d0773ca350b1b8da
cmb69 <cmbecker69@gmx.de>
2014-05-15 11:36:02 +0000

    added German localization

---
+++ languages/de.php
@@ -0,0 +1,6 @@
+<?php
+
+$plugin_tx['filter']['label_all']="Alles";
+$plugin_tx['filter']['cf_categories']="Eine durch Kommas getrennte Liste verfügbarer Kategorien.";
+
+?>
--- languages/en.php
+++ languages/en.php
@@ -1,5 +1,6 @@
 <?php
 
 $plugin_tx['filter']['label_all']="all";
+$plugin_tx['filter']['cf_categories']="A comma separated list of available categories.";
 
 ?>
